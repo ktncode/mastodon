0d09d9fb44713a047ddb34c8933c626d55188688
Eugen Rochko <eugen@zeonfederated.com>
2021-07-10 17:58:41 +0200

    Add `configuration` attribute to `GET /api/v1/instance` (mastodon/mastodon#16485)

--- app/validators/status_length_validator.rb
+++ app/validators/status_length_validator.rb
@@ -3,7 +3,7 @@
 class StatusLengthValidator < ActiveModel::Validator
   MAX_CHARS = (ENV['MAX_TOOT_CHARS'] || 500).to_i
   URL_PLACEHOLDER_CHARS = 23
-  URL_PLACEHOLDER = 'x' * 23
+  URL_PLACEHOLDER = "\1#{'x' * URL_PLACEHOLDER_CHARS}"
 
   def validate(status)
     return unless status.local? && !status.reblog?
