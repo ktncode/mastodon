c7bcc6ae058cc811a74c0cbb91f6fb6c60d704b5
Matt Jankowski <matt@jankowski.online>
2024-01-22 08:55:37 -0500

    Add `Account.matches_uri_prefix` scope and use in activitypub/followers_synchronizations controller (#28820)

--- app/models/account.rb
+++ app/models/account.rb
@@ -134,6 +134,7 @@ class Account < ApplicationRecord
   scope :groups, -> { where(actor_type: 'Group') }
   scope :without_groups, -> { where.not(actor_type: 'Group') }
   scope :alphabetic, -> { order(domain: :asc, username: :asc) }
+  scope :matches_uri_prefix, ->(value) { where(arel_table[:uri].matches("#{sanitize_sql_like(value)}/%", false, true)).or(where(uri: value)) }
   scope :matches_username, ->(value) { where('lower((username)::text) LIKE lower(?)', "#{value}%") }
   scope :matches_display_name, ->(value) { where(arel_table[:display_name].matches("#{value}%")) }
   scope :matches_domain, ->(value) { where(arel_table[:domain].matches("%#{value}%")) }
