a996cf9393846e5648f78e3915b1a2c8e6b94d39
Claire <claire.github-309c@sitedethib.com>
2024-07-04 16:26:49 +0200

    Merge pull request from GHSA-58x8-3qxw-6hm7
    * Fix insufficient permission checking for public timeline endpoints
    
    Note that this changes unauthenticated access failure code from 401 to 422
    
    * Add more tests for public timelines
    
    * Require user token in `/api/v1/statuses/:id/translate` and `/api/v1/scheduled_statuses`

--- spec/controllers/api/v1/timelines/tag_controller_spec.rb
+++ spec/controllers/api/v1/timelines/tag_controller_spec.rb
@@ -5,7 +5,9 @@ require 'rails_helper'
 describe Api::V1::Timelines::TagController do
   render_views
 
-  let(:user) { Fabricate(:user, account: Fabricate(:account, username: 'alice')) }
+  let(:user)   { Fabricate(:user) }
+  let(:scopes) { 'read:statuses' }
+  let(:token)  { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
 
   before do
     allow(controller).to receive(:doorkeeper_token) { token }
@@ -19,10 +21,24 @@ describe Api::V1::Timelines::TagController do
         PostStatusService.new.call(user.account, text: 'It is a #test')
       end
 
-      it 'returns http success' do
-        get :show, params: { id: 'test' }
-        expect(response).to have_http_status(200)
-        expect(response.headers['Link'].links.size).to eq(2)
+      context 'without an access token' do
+        let(:token) { nil }
+
+        it 'returns http unprocessable entity' do
+          subject
+
+          expect(response).to have_http_status(422)
+        end
+      end
+
+      context 'with an application access token, not bound to a user' do
+        let(:token) { Fabricate(:accessible_access_token, resource_owner_id: nil, scopes: scopes) }
+
+        it 'returns http unprocessable entity' do
+          subject
+
+          expect(response).to have_http_status(422)
+        end
       end
     end
   end
