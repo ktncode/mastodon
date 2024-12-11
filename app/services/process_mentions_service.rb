ae1c2f8ba70f36e1086512dc9518016dea23dcf1
noellabo <noel.yoshiba@gmail.com>
2024-12-12 07:59:11 +0900

    Add ability to deliver to default group

--- app/services/process_mentions_service.rb
+++ app/services/process_mentions_service.rb
@@ -14,6 +14,14 @@ class ProcessMentionsService < BaseService
     @status  = status
     mentions = []
 
+    if ENV.fetch('DEFAULT_GROUP', nil).present? && status.distributable?
+      mentioned_account = Account.find_local(ENV.fetch('DEFAULT_GROUP'))
+
+      if !mentioned_account.nil? && mentioned_account.group?
+        mentions << mentioned_account.mentions.where(status: status).first_or_create(status: status)
+      end
+    end
+
     status.text = status.text.gsub(Account::MENTION_RE) do |match|
       username, domain = Regexp.last_match(1).split('@')
 
