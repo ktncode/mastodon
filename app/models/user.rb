7047dba5cb4e6063e364c1a2903c42377d0d4915
noellabo <noel.yoshiba@gmail.com>
2025-02-18 21:52:46 +0900

    Fix email scheduler

--- app/models/user.rb
+++ app/models/user.rb
@@ -112,7 +112,7 @@ class User < ApplicationRecord
   scope :active, -> { confirmed.where(arel_table[:current_sign_in_at].gteq(ACTIVE_DURATION.ago)).joins(:account).where(accounts: { suspended_at: nil }) }
   scope :matches_email, ->(value) { where(arel_table[:email].matches("#{value}%")) }
   scope :matches_ip, ->(value) { left_joins(:session_activations).where('users.current_sign_in_ip <<= ?', value).or(left_joins(:session_activations).where('users.sign_up_ip <<= ?', value)).or(left_joins(:session_activations).where('users.last_sign_in_ip <<= ?', value)).or(left_joins(:session_activations).where('session_activations.ip <<= ?', value)) }
-  scope :emailable, -> { confirmed.enabled.joins(:account).merge(Account.searchable) }
+  scope :emailable, -> { confirmed.enabled.joins(:account).merge(Account.without_suspended.where(moved_to_account_id: nil)) }
 
   before_validation :sanitize_languages
   before_validation :sanitize_time_zone
